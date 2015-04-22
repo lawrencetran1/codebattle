93cfc93f0c645e529b5ac34e8901034cc96d4876
Lawrence Tran <lawrencetran1@gmail.com>
2015-04-22 13:57:00 -0700

    Adds loop to view all challenges for current_user

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,8 @@
 class Challenge < ActiveRecord::Base
 	belongs_to :user
-	# validates :user_id, presence: true
-	# validates :name, presence: true
-	# validates :instructions, presence: true
-	# validates :example, presence: true
-	# validates :category, presence: true
+	validates :user_id, presence: true
+	validates :name, presence: true, length: { maximum: 32 }
+	validates :instructions, presence: true
+	validates :example, presence: true
+	validates :category, presence: true
 end
