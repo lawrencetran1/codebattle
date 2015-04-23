26aff18fbd05ca800a6ac0e05597d30d72235bcf
Lawrence Tran <lawrencetran1@gmail.com>
2015-04-22 21:56:00 -0700

    Adds full screen home page

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,6 +3,10 @@ class ChallengesController < ApplicationController
     @challenge = Challenge.new
   end
 
+  def edit
+    @challenge = Challenge.find(params[:id])
+  end
+
   def create
   	@challenge = Challenge.new(challenge_params)
     @challenge.user = current_user
@@ -12,6 +16,21 @@ class ChallengesController < ApplicationController
       flash[:alert] = @challenge.errors.full_messages
   		render "new"
   	end
+  end
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    if @challenge.update_attributes(challenge_params)
+      redirect_to current_user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    challenge = Challenge.find(params[:id])
+    challenge.destroy
+    redirect_to current_user
   end
 
   private
